4f8d8064e831e8d97c6f60e5e50f98bcd3c3e769
Cristián Aguirre <82728271+CristianAguirre-VICOSCIENCE@users.noreply.github.com>
2022-03-19 01:12:37 -0300

    first commit

---
+++ server_test/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Este servidor funciona
+        </p>
+        <p>
+          Free Taiwan
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
